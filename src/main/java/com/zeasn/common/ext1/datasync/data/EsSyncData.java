6d75856ec31868e677e68e0a04550d255cdc93b5
zuodan <yudingling@126.com>
2019-09-04 18:51:39 +0800

    update

--- src/main/java/com/zeasn/common/ext1/datasync/data/EsSyncData.java
+++ src/main/java/com/zeasn/common/ext1/datasync/data/EsSyncData.java
@@ -4,15 +4,78 @@ public class EsSyncData extends SyncData {
 	private static final long serialVersionUID = 1815560778676010106L;
 	
 	private ActionType action;
+	private String cls;
+	private String index;
+	private String type;
+	private String id;
+	private String parentId;
+	private Long version;
+	private String mappingJson;
 	private String serializedValue;
 	private boolean upsert = false;
-	
+
 	public ActionType getAction() {
 		return action;
 	}
 
 	public void setAction(ActionType action) {
 		this.action = action;
+	}
+
+	public String getCls() {
+		return cls;
+	}
+
+	public void setCls(String cls) {
+		this.cls = cls;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public String getMappingJson() {
+		return mappingJson;
+	}
+
+	public void setMappingJson(String mappingJson) {
+		this.mappingJson = mappingJson;
 	}
 
 	public String getSerializedValue() {
@@ -35,10 +98,20 @@ public class EsSyncData extends SyncData {
 		super();
 	}
 	
-	public EsSyncData(String appName, String groupName, long deferMilliseconds, ActionType action, String serializedValue, boolean upsert){
+	public EsSyncData(String appName, String groupName, long deferMilliseconds,
+			ActionType action, String cls, String index, String type,
+			String id, String parentId, Long version, String mappingJson,
+			String serializedValue, boolean upsert) {
 		super(appName, groupName, deferMilliseconds);
 		
 		this.action = action;
+		this.cls = cls;
+		this.index = index;
+		this.type = type;
+		this.id = id;
+		this.parentId = parentId;
+		this.version = version;
+		this.mappingJson = mappingJson;
 		this.serializedValue = serializedValue;
 		this.upsert = upsert;
 	}
