80e2f85c747e37a214f500552816ccca69d0b591
zuodan <yudingling@126.com>
2019-10-24 18:23:19 +0800

    update

--- src/main/java/com/zeasn/common/ext1/datasync/data/EsSyncData.java
+++ src/main/java/com/zeasn/common/ext1/datasync/data/EsSyncData.java
@@ -1,5 +1,7 @@
 package com.zeasn.common.ext1.datasync.data;
 
+import java.util.Set;
+
 public class EsSyncData extends SyncData {
 	private static final long serialVersionUID = 1815560778676010106L;
 	
@@ -11,6 +13,13 @@ public class EsSyncData extends SyncData {
 	private Long version;
 	private String mappingJson;
 	private String serializedValue;
+	
+	private Set<String> indexes;
+	private Integer pageSize;
+	private Long scrollTimeInMillis;
+	private String serializedQueryBuilder;
+	private String serializedScript;
+	
 	private boolean upsert = false;
 
 	public ActionType getAction() {
@@ -85,6 +94,46 @@ public class EsSyncData extends SyncData {
 		this.upsert = upsert;
 	}
 
+	public Set<String> getIndexes() {
+		return indexes;
+	}
+
+	public void setIndexes(Set<String> indexes) {
+		this.indexes = indexes;
+	}
+
+	public String getSerializedQueryBuilder() {
+		return serializedQueryBuilder;
+	}
+
+	public void setSerializedQueryBuilder(String serializedQueryBuilder) {
+		this.serializedQueryBuilder = serializedQueryBuilder;
+	}
+
+	public String getSerializedScript() {
+		return serializedScript;
+	}
+
+	public void setSerializedScript(String serializedScript) {
+		this.serializedScript = serializedScript;
+	}
+
+	public Integer getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public Long getScrollTimeInMillis() {
+		return scrollTimeInMillis;
+	}
+
+	public void setScrollTimeInMillis(Long scrollTimeInMillis) {
+		this.scrollTimeInMillis = scrollTimeInMillis;
+	}
+
 	public EsSyncData(){
 		super();
 	}
@@ -104,6 +153,37 @@ public class EsSyncData extends SyncData {
 		this.mappingJson = mappingJson;
 		this.serializedValue = serializedValue;
 		this.upsert = upsert;
+	}
+	
+	public EsSyncData(String appName, String groupName, long deferMilliseconds,
+			ActionType action, String index, String type, Integer pageSize, Long scrollTimeInMillis, String serializedQueryBuilder) {
+		super(appName, groupName, deferMilliseconds);
+		
+		this.action = action;
+		this.index = index;
+		this.type = type;
+		this.pageSize = pageSize;
+		this.scrollTimeInMillis = scrollTimeInMillis;
+		this.serializedQueryBuilder = serializedQueryBuilder;
+	}
+	
+	public EsSyncData(String appName, String groupName, long deferMilliseconds,
+			ActionType action, Set<String> indexes, String serializedQueryBuilder) {
+		super(appName, groupName, deferMilliseconds);
+		
+		this.action = action;
+		this.indexes = indexes;
+		this.serializedQueryBuilder = serializedQueryBuilder;
+	}
+	
+	public EsSyncData(String appName, String groupName, long deferMilliseconds,
+			ActionType action, Set<String> indexes, String serializedScript, String serializedQueryBuilder) {
+		super(appName, groupName, deferMilliseconds);
+		
+		this.action = action;
+		this.indexes = indexes;
+		this.serializedScript = serializedScript;
+		this.serializedQueryBuilder = serializedQueryBuilder;
 	}
 
 	public enum ActionType{
